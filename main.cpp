92b551790b73b639f8fc013047b5f8a78d94e3a2
bluecrown5@ymobile.ne.jp <bluecrown2525@gmail.com>
2024-03-17 05:15:45 +0900

    MatrixMathのヘッダ及びcppファイルの追加 Matrix4x4の計算に関する関数の実装

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <Novice.h>
+#include "MyMath/MatrixMath.h"
 
-const char kWindowTitle[] = "AL2_02_イシカワタケシ";
+const char kWindowTitle[] = "イシカワタケシ_MT3_00_02";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Matrix4x4 m1 = {
+		3.2f,0.7f,9.6f,4.4f,
+		5.5f,1.3f,7.8f,2.1f,
+		6.9f,8.0f,2.6f,1.0f,
+		0.5f,7.2f,5.1f,3.3f
+	};
+
+	Matrix4x4 m2 = {
+		4.1f,6.5f,3.3f,2.2f,
+		8.8f,0.6f,9.9f,7.7f,
+		1.1f,5.5f,6.6f,0.0f,
+		3.3f,9.9f,8.8f,2.2f
+	};
+
+	Matrix4x4 resultAdd = Add(m1, m2);
+	Matrix4x4 resultSubtract = Subtract(m1, m2);
+	Matrix4x4 resultMultiply = Multiply(m1, m2);
+	Matrix4x4 inverseM1 = Inverse(m1);
+	Matrix4x4 inverseM2 = Inverse(m2);
+	Matrix4x4 transposeM1 = Transpose(m1);
+	Matrix4x4 transposeM2 = Transpose(m2);
+	Matrix4x4 identity = MakeIdentity4x4();
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -32,6 +56,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatirxScreenPrintf(0, 0, resultAdd, "Add");
+		MatirxScreenPrintf(0, 20 * 5, resultSubtract, "Subtract");
+		MatirxScreenPrintf(0, 20 * 5 * 2, resultMultiply, "Multiply");
+		MatirxScreenPrintf(0, 20 * 5 * 3, inverseM1, "inverseM1");
+		MatirxScreenPrintf(0, 20 * 5 * 4, inverseM2, "inverseM2");
+		MatirxScreenPrintf(60 * 5, 0, transposeM1, "transposeM1");
+		MatirxScreenPrintf(60 * 5, 20 * 5, transposeM2, "transposeM2");
+		MatirxScreenPrintf(60 * 5, 20 * 5 * 2, identity, "identity");
 
 		///
 		/// ↑描画処理ここまで
