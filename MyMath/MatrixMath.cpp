ed71dcfdddb244357bd10cfa7c002fbb403864fa
IshikawaTakeshi <K023G0065@g.neec.ac.jp>
2024-04-12 16:18:27 +0900

    Add,Subtractの計算方法の変更

--- MyMath/MatrixMath.cpp
+++ MyMath/MatrixMath.cpp
@@ -3,23 +3,23 @@
 
 //行列の加法
 Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 result = {
-		m1.m[0][0] + m2.m[0][0],m1.m[0][1] + m2.m[0][1],m1.m[0][2] + m2.m[0][2],m1.m[0][3] + m2.m[0][3],
-		m1.m[1][0] + m2.m[1][0],m1.m[1][1] + m2.m[1][1],m1.m[1][2] + m2.m[1][2],m1.m[1][3] + m2.m[1][3],
-		m1.m[2][0] + m2.m[2][0],m1.m[2][1] + m2.m[2][1],m1.m[2][2] + m2.m[2][2],m1.m[2][3] + m2.m[2][3],
-		m1.m[3][0] + m2.m[3][0],m1.m[3][1] + m2.m[3][1],m1.m[3][2] + m2.m[3][2],m1.m[3][3] + m2.m[3][3]
-	};
+	Matrix4x4 result;
+	for (int row = 0; row < 4; row++) {
+		for (int column = 0; column < 4; column++) {
+			result.m[row][column] = m1.m[row][column] + m2.m[row][column];
+		}
+	}
 	return result;
 }
 
 //行列の減法
 Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 result = {
-		m1.m[0][0] - m2.m[0][0],m1.m[0][1] - m2.m[0][1],m1.m[0][2] - m2.m[0][2],m1.m[0][3] - m2.m[0][3],
-		m1.m[1][0] - m2.m[1][0],m1.m[1][1] - m2.m[1][1],m1.m[1][2] - m2.m[1][2],m1.m[1][3] - m2.m[1][3],
-		m1.m[2][0] - m2.m[2][0],m1.m[2][1] - m2.m[2][1],m1.m[2][2] - m2.m[2][2],m1.m[2][3] - m2.m[2][3],
-		m1.m[3][0] - m2.m[3][0],m1.m[3][1] - m2.m[3][1],m1.m[3][2] - m2.m[3][2],m1.m[3][3] - m2.m[3][3]
-	};
+	Matrix4x4 result;
+	for (int row = 0; row < 4; row++) {
+		for (int column = 0; column < 4; column++) {
+			result.m[row][column] = m1.m[row][column] - m2.m[row][column];
+		}
+	}
 	return result;
 }
 
@@ -27,10 +27,10 @@ Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 result;
 	for (int row = 0; row < 4; row++){
-		for (int colmn = 0; colmn < 4; colmn++){
-			result.m[row][colmn] = 0;
+		for (int column = 0; column < 4; column++){
+			result.m[row][column] = 0;
 			for (int k = 0; k < 4; k++){
-				result.m[row][colmn] += m1.m[row][k] * m2.m[k][colmn];
+				result.m[row][column] += m1.m[row][k] * m2.m[k][column];
 			}
 		}
 	}
